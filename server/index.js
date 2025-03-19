69173f1699c2642e56b015c8af46035ca791c1be
Rishab Raj <rishabrajkmr4567@gmail.com>
2025-03-20 00:19:00 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -35,6 +35,10 @@ const io = new Server(server, {
     }
 });
 
+app.get('/', (req, res) => {
+    res.json({ message: 'Server is running' });
+})
+
 io.on("connection", (socket) =>{
     console.log("User connected");
 
